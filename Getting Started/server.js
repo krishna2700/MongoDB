a83ce3593b84183de4110db78bab024e895662e4
krishna2700 <krishnaruparelia0207@gmail.com>
2024-07-07 11:04:13 +0530

    CRUD operation in Mongodb

--- Getting Started/server.js
+++ Getting Started/server.js
@@ -1,68 +1,175 @@
 const express = require("express");
+const {
+  MongoClient,
+  ServerApiVersion,
+  ObjectId,
+  Decimal128,
+} = require("mongodb");
 const app = express();
-const port = 3690;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const PORT = 3690;
+//Connect to mongodb
 
-// * Connet to MongoDB
-// ? 1 Create the client
+const mongodbURL =
+  "mongodb+srv://krishnaruparelia0207:DxrcAvKIvk5KgRpU@krishnagettingstarted.eqdddjp.mongodb.net/students-database";
 
-const client = new MongoClient(
-  `mongodb+srv://krishnaruparelia0207:DxrcAvKIvk5KgRpU@krishnagettingstarted.eqdddjp.mongodb.net/students-database`,
-  {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  }
-);
+const client = new MongoClient(mongodbURL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-// * Function to connect
-
-const connectDb = async () => {
+const connectDB = async () => {
   try {
     await client.connect();
-    console.log("MongoDb connected");
-    // *1 Database name (school)
+    console.log("MongoDB Connected successfully");
+    //1.Create your db (school)
     const database = client.db("dasnadasSchool");
-    // * 2 Collection name (students)
+    //2.Collections
     const students = database.collection("students");
-    // *3 Actual documnets () using insert one
+    const books = database.collection("books");
+    //!----CREATE----
+    //! Create document (any student) - insertOne
+
     // const result = await students.insertOne({
-    //   name: "Krishna",
-    //   age: 24,
-    //   subject: ["Maths", "Chemistry", "Physics"],
+    //   name: "Agnes",
+    //   age: 20,
+    //   subjects: ["Math", "Physics"],
+    // });
+    // console.log(result);
+    //! Using insertMany
+    // const results = await students.insertMany([
+    //   {
+    //     name: "John",
+    //     age: 26,
+    //     grade: "B",
+    //     pass: false,
+    //     subjects: ["Chemistry", "Biology"],
+    //   },
+    //   {
+    //     name: "Joseph",
+    //     grade: "B",
+    //     pass: false,
+    //     age: 30,
+    //     subjects: ["Chemistry1", "Biology1"],
+    //   },
+    //   {
+    //     name: "Prince",
+    //     grade: "C",
+    //     pass: true,
+    //     age: 30,
+    //     subjects: ["Chemistry1", "Biology1"],
+    //   },
+    // ]);
+    // console.log(results);
+    //!----READ OPERATIONS----
+    //! Find()
+    // const resultsCursor = students.find();
+    // const results = await resultsCursor.toArray();
+    // console.log(results);
+    //!----READ OPERATIONS----
+    //! FindOne()
+    // const result = await students.findOne({
+    //   age: 27,
     // });
 
-    // *3 Actual documnets () using insert many
-    const result = await students.insertMany([
-      {
-        name: "John",
-        age: 26,
-        grade: "A",
-        pass: true,
-        subjects: ["Maths", "Chemistry", "Physics"],
-      },
-      {
-        name: "Jane",
-        age: 27,
-        grade: "F",
-        pass: false,
-        subjects: ["Maths", "Chemistry", "Physics"],
-      },
-    ]);
+    //!----UPDATE OPERATIONS----
+    //! updateOne()
+    // const resultArray = students.find();
+    // const result = await resultArray.toArray();
+    // const result = await students.updateOne(
+    //   { name: "Jade" },
+    //   { $set: { name: "Krishna" } }
+    // );
+    // console.log(result);
+
+    //! updateMany()
+    // const result = await students.updateMany(
+    //   { grade: "A" },
+    //   { $set: { pass: true } }
+    // );
+
+    //! findOneAndUpdate()
+    const result = await students.findOneAndUpdate(
+      { name: "Jade" },
+      { $set: { name: "Krishna" } }
+    );
+    // console.log(result);
+
+    //!----DELETE OPERATIONS----
+    //! deleteOne()
+    // const result = await students.deleteOne({
+    //   name: "Emmanuel2",
+    // });
+    // console.log(result);
+    //! deleteMany()
+    // const result = await students.deleteMany({
+    //   grade: "B",
+    // });
+    // console.log(result);
+    //! findOneAndDelete()
+    // const result = await students.findOneAndDelete({
+    //   name: "Prince",
+    // });
+    // console.log(result);
+
+    //!----BULK WRITE OPERATIONS----
+    //! insert
+    //! Update
+    //! Delete
+    // const bulkWriteOperations = [
+    //   // Creation stage
+    //   {
+    //     insertOne: {
+    //       document: {
+    //         name: "John",
+    //         age: 20,
+    //         grade: "A",
+    //       },
+    //     },
+    //   },
+    //   // update stage
+    //   {
+    //     updateOne: {
+    //       filter: { name: "John" },
+    //       update: { $set: { grade: "B" } },
+    //     },
+    //   },
+    //   // delete stage
+    //   {
+    //     deleteOne: {
+    //       filter: { name: "John" },
+    //     },
+    //   },
+    // ];
+    // const result = await students.bulkWrite(bulkWriteOperations);
+    // console.log(result);
+    //!----DATA TYPES OPERATIONS----
+    // const result = await books.insertOne({
+    //   _id: new ObjectId(), //object
+    //   title: "To Kill a Mockingbird", //string
+    //   author: {
+    //     firstname: "Harper", //Embeded //string'
+    //     lastname: "Lee",
+    //   },
+    //   isAvailable: true, //Boolean
+    //   price: Decimal128.fromString("10.99"), //Decimal 128
+    //   tags: ["Classic", "Literature"], //Array
+    //   ratings: [4.5, 4.0, 5.0], //Array of doubles
+    //   publisher: {
+    //     name: "JB Lippicott",
+    //     founded: "1982",
+    //     isActive: true, // Boolean
+    //   },
+    // });
     console.log(result);
   } catch (error) {
     console.log(error);
   }
 };
+//Run the function
+connectDB();
 
-// * call the fuction
-
-connectDb();
-
-//  Start the server
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+//!Start the server
+app.listen(PORT, console.log(`The server is up and running on port ${PORT}`));

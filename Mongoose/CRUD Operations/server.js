ee38db39d2fef6caee416328f8d18edfb9f3eabb
krishna2700 <krishnaruparelia0207@gmail.com>
2024-07-10 09:25:07 +0530

    Create Operator

---
+++ Mongoose/CRUD Operations/server.js
@@ -0,0 +1,139 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// PORT
+const PORT = 3690 || process.env.PORT;
+// Instance of express
+const app = express();
+// Connect to mongodb
+const URL =
+  "mongodb+srv://krishnaruparelia0207:DxrcAvKIvk5KgRpU@krishnagettingstarted.eqdddjp.mongodb.net/Krishna-DB";
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(URL);
+    console.log("MongoDB Connected successfully");
+  } catch (error) {
+    console.log(`Error Connecting to MongoDB ${error}`);
+  }
+};
+// Run the function
+connectToDB();
+// ! Design schema
+const userProfileSchema = new mongoose.Schema({
+  username: String, //string
+  age: Number, //number
+  birthday: Date, //Date
+  isActive: Boolean, //Boolean
+  hobbies: [String], //Arrays of strings
+  objectID: mongoose.Schema.Types.ObjectId, //ObjectID
+  address: {
+    street: String,
+    city: String,
+    postalCode: Number,
+  }, //Embedded Document
+  customData: mongoose.Schema.Types.Mixed,
+});
+// ! Compile the Schema to form the model
+const user = mongoose.model("userProfile", userProfileSchema);
+
+//  ! =========== CRUD Operations ============
+// ? Create Doc
+// *.save()
+const newUser = new user({
+  username: "Krishna",
+  age: 24,
+  birthday: new Date("2000-07-02"),
+  isActive: true,
+  hobbies: ["Coding", "Cooking", "Reading"],
+  address: {
+    street: "Main St",
+    city: "Boston",
+    postalCode: 1234,
+  },
+  customData: {
+    country: "USA",
+    state: "MA",
+    zip: 1234,
+  },
+});
+// newUser
+//   .save()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// *.create()
+// user
+//   .create({
+//     username: "Krishna Ruparelia",
+//     age: 24,
+//     birthday: new Date("2000-07-02"),
+//     isActive: true,
+//     hobbies: ["Coding", "Cooking", "Reading"],
+//     address: {
+//       street: "Main St",
+//       city: "Boston",
+//       postalCode: 1234,
+//     },
+//     customData: {
+//       country: "USA",
+//       state: "MA",
+//       zip: 1234,
+//     },
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// *.insertMany()
+user
+  .insertMany([
+    {
+      username: "Ruparelia",
+      age: 24,
+      birthday: new Date("2000-07-02"),
+      isActive: true,
+      hobbies: ["Coding", "Cooking", "Reading"],
+      address: {
+        street: "Main St",
+        city: "Boston",
+        postalCode: 1234,
+      },
+      customData: {
+        country: "USA",
+        state: "MA",
+        zip: 1234,
+      },
+    },
+    {
+      username: "Krishna Das",
+      age: 24,
+      birthday: new Date("2000-07-02"),
+      isActive: true,
+      hobbies: ["Coding", "Cooking", "Reading"],
+      address: {
+        street: "Main St",
+        city: "Boston",
+        postalCode: 1234,
+      },
+      customData: {
+        country: "USA",
+        state: "MA",
+        zip: 1234,
+      },
+    },
+  ])
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// Start the server
+app.listen(PORT, console.log(`The server is up and running on port ${PORT}`));
